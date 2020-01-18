01a048b4d254f107a183a385fb50079412ad7c1a
Martin Sch <amrastasu@gmail.com>
2020-01-18 18:51:20 -0300

    add select month field

--- lib/app/modules/home/home_page.dart
+++ lib/app/modules/home/home_page.dart
@@ -1,28 +1,44 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
-import 'package:intl/intl.dart';
 import 'home_controller.dart';
 
 class HomePage extends StatefulWidget {
   final String title;
+
   const HomePage({Key key, this.title = "Home"}) : super(key: key);
+
   @override
   _HomePageState createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
   var homeController = HomeController();
-  DateTime selectedDate = DateTime.now();
+  final List<String> _months = [
+    "Janeiro",
+    "Fevereiro",
+    "Março",
+    "Abril",
+    "Maio",
+    "Junho",
+    "Julho",
+    "Agosto",
+    "Setembro",
+    "Outubro",
+    "Novembro",
+    "Dezembro"
+  ];
+  String _selectMonth;
+  String _selectedDate = "Janeiro";
 
-  Future<Null> _selectDate(BuildContext context) async {
-    print(new DateFormat.MMMM().format(new DateTime.now()));
-    final DateTime picked = await showDatePicker(
-        context: context,
-        initialDate: selectedDate,
-        firstDate: DateTime(2015, 8),
-        lastDate: DateTime(2101));
-    //if (picked != null && picked != selectedDate)
-  }
+  // Future<Null> _selectDate(BuildContext context) async {
+  //   print(new DateFormat.MMMM().format(new DateTime.now()));
+  //   final DateTime picked = await showDatePicker(
+  //       context: context,
+  //       initialDate: selectedDate,
+  //       firstDate: DateTime(2015, 8),
+  //       lastDate: DateTime(2101));
+  //   if (picked != null && picked != selectedDate)
+  // }
 
   @override
   Widget build(BuildContext context) {
@@ -33,14 +49,19 @@ class _HomePageState extends State<HomePage> {
         body: SingleChildScrollView(
           child: Column(
             children: <Widget>[
-              RaisedButton(
-                child: Text("Selecione um dia"),
-                onPressed: () => {_selectDate(context)},
-              ),
+              new DropdownButton<String>(
+                  value: _selectedDate,
+                  items: _months.map((String value) {
+                    return new DropdownMenuItem(
+                        value: value, child: new Text("${value}"));
+                  }).toList(),
+                  onChanged: (String value) {
+                    onMonthChange(value);
+                  }),
               RaisedButton(
                 child: Text("Request"),
                 onPressed: () =>
-                    {homeController.searchByThisDay("1 de janeiro")},
+                    {homeController.searchByThisDay("25 de janeiro")},
               ),
               Observer(
                 builder: (_) => Text('${homeController.text}'),
@@ -48,5 +69,11 @@ class _HomePageState extends State<HomePage> {
             ],
           ),
         ));
+  }
+
+  void onMonthChange(value) {
+    setState(() {
+      _selectedDate = value;
+    });
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:nesse_dia/app/app_module.dart';
 import 'package:flutter_modular/flutter_modular.dart';
-import 'package:intl/intl.dart';
-import 'package:intl/date_symbol_data_local.dart';
 
-void main() => initializeDateFormatting("fr_FR", null)
-    .then((_) => runApp(ModularApp(module: AppModule())));
+void main() => runApp(ModularApp(module: AppModule()));
